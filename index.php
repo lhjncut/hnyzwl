2b289c9af47fab0535f364ffd2ba4ed989188456
LiuHaijian <lhjncut@126.com>
2017-02-20 08:56:35 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "../hm.baidu.com/hm.js-1b2e6c923418844114540e83a030c674.js"/*tpa=https://hm.baidu.com/hm.js?1b2e6c923418844114540e83a030c674*/;
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>广州南广物流有限公司</title><meta name="keywords" content="广州南广物流有限公司"><meta name="description" content="广州南广物流有限公司"><link href="templates/web/images/css/style.css"  rel="stylesheet" type="text/css" /><script type="text/javascript" src="templates/web/images/js/jquery-1.8.3.min.js" ></script><script src="templates/web/images/js/function.js"  type="text/javascript"></script><script src="templates/web/images/js/jquery.superslide.2.1.js"  type="text/javascript"></script><script type="text/javascript" src="templates/web/images/js/jquery.tools.min.js" ></script><script type="text/javascript" src="templates/web/images/js/main.js" ></script></head>
+<body>
+
+<?php include 'header.php';?>
+
+<div class="banner"><ul class="slides"><li style="background:url(upfiles/201701/03/adadb816bbee4f6da.jpg)/*tpa=http://www.gznanguang.com/upfiles/201701/03/adadb816bbee4f6da.jpg*/ 50% 0 no-repeat; height:580px"></li><li style="background:url(upfiles/201701/03/a81b9d99459a1735e.jpg)/*tpa=http://www.gznanguang.com/upfiles/201701/03/a81b9d99459a1735e.jpg*/ 50% 0 no-repeat; height:580px"></li></ul></div>
+
+<script src="templates/web/images/js/jquery.flexslider-min.js" ></script><script>
+    $(function(){
+        $('.banner').flexslider({
+            directionNav: true,
+            pauseOnAction: false
+        });
+    });
+    </script>
+	
+	
+	<div class="one" style="padding:130px 0 0px 0;"><div class="list"><div class="mytab"><a><img src="templates/web/images/01_03.png"  width="84" height="82" class="rgb"/></a></div><div class="title"><span>仓储展示</span><strong>S</strong>ervice advantage​​ 
+    </div><ul><li><a  ><div class="ups">仓库实景图 <span></span><img src="upfiles/201701/04/406_thumb.jpg"  width="130" height="130" class="rgb"/></div></a></li><li><a  ><div class="ups">仓库实景图 <span></span><img src="upfiles/201701/04/407_thumb.jpg"  width="130" height="130" class="rgb"/></div></a></li><li><a  ><div class="ups">仓库实景图 <span></span><img src="upfiles/201701/04/408_thumb.jpg"  width="130" height="130" class="rgb"/></div></a></li><li><a  ><div class="ups">仓库实景图 <span></span><img src="upfiles/201701/04/409_thumb.jpg"  width="130" height="130" class="rgb"/></div></a></li></ul></div></div>
+	
+	
+	<div class="two"><div class="list"><div class="mytab"><a><img src="templates/web/images/02_03.png"  width="84" height="81"  class="rgb"/></a></div><div class="title1"><span><a href="-c=msg&ts=aboutus.htm" >关于我们</a></span><strong>A</strong>bout 
+    </div><div class="aimg"><img src="templates/web/images/p1.jpg"  width="283" height="200" /></div><div class="anew"><p style="text-indent: 2em; line-height: 1.75em;">河南省远征物流有限公司成立于2008年，是一家经郑州市交通.工商等部门批准注册成立的仓储，货物运输，货物中转综合性物流公司。公司自成立至今，连续多年在交通.物流协会等主管部门评比中获得“重合同，守信誉”企业，“诚信物流企业”等荣誉称号。。</p></div></div></div>
+	
+	
+	
+
+		
+		
+		<div class="six"><div class="mytab"><a><img src="templates/web/images/03_03.png"  width="84" height="81"  class="rgb"/></a></div><div class="title1"><span><a href="index.htm" >公司集锦</a></span><strong>S</strong>uccessful case 
+  </div><div class="ind_03"><div  class="k1"><script type="text/javascript" src="statics/js/msclass.js" ></script><div id="nana" style="margin:0 0 0 10px;"><table class="media_grid" cellspacing="4" width="100%"><tr><td><div class="newprod_list" style="padding:10px 10px 0 0"><div class="newprod_pic" style="border:1px solid #b3b3b3; padding: 0 0 10px 0;"><a   title="仓库实景图"><img name="picautozoom" border="0" src="upfiles/201701/04/406_thumb.jpg"  alt="仓库实景图" class="prodthumb" width="150" height="150"/></a><div class="newprod_name" style="text-align:center; padding:10px 0 0 0"><a   title="仓库实景图" >仓库实景图</a></div></div><div class="blankbar1"></div></div></td><td><div class="newprod_list" style="padding:10px 10px 0 0"><div class="newprod_pic" style="border:1px solid #b3b3b3; padding: 0 0 10px 0;"><a   title="仓库实景图"><img name="picautozoom" border="0" src="upfiles/201701/04/407_thumb.jpg"  alt="仓库实景图" class="prodthumb" width="150" height="150"/></a><div class="newprod_name" style="text-align:center; padding:10px 0 0 0"><a   title="仓库实景图" >仓库实景图</a></div></div><div class="blankbar1"></div></div></td><td><div class="newprod_list" style="padding:10px 10px 0 0"><div class="newprod_pic" style="border:1px solid #b3b3b3; padding: 0 0 10px 0;"><a   title="仓库实景图"><img name="picautozoom" border="0" src="upfiles/201701/04/408_thumb.jpg"  alt="仓库实景图" class="prodthumb" width="150" height="150"/></a><div class="newprod_name" style="text-align:center; padding:10px 0 0 0"><a   title="仓库实景图" >仓库实景图</a></div></div><div class="blankbar1"></div></div></td><td><div class="newprod_list" style="padding:10px 10px 0 0"><div class="newprod_pic" style="border:1px solid #b3b3b3; padding: 0 0 10px 0;"><a   title="仓库实景图"><img name="picautozoom" border="0" src="upfiles/201701/04/409_thumb.jpg"  alt="仓库实景图" class="prodthumb" width="150" height="150"/></a><div class="newprod_name" style="text-align:center; padding:10px 0 0 0"><a   title="仓库实景图" >仓库实景图</a></div></div><div class="blankbar1"></div></div></td><td><div class="newprod_list" style="padding:10px 10px 0 0"><div class="newprod_pic" style="border:1px solid #b3b3b3; padding: 0 0 10px 0;"><a   title="仓库实景图"><img name="picautozoom" border="0" src="upfiles/201701/04/410_thumb.jpg"  alt="仓库实景图" class="prodthumb" width="150" height="150"/></a><div class="newprod_name" style="text-align:center; padding:10px 0 0 0"><a   title="仓库实景图" >仓库实景图</a></div></div><div class="blankbar1"></div></div></td><td><div class="newprod_list" style="padding:10px 10px 0 0"><div class="newprod_pic" style="border:1px solid #b3b3b3; padding: 0 0 10px 0;"><a   title="仓库实景图"><img name="picautozoom" border="0" src="upfiles/201701/04/411_thumb.jpg"  alt="仓库实景图" class="prodthumb" width="150" height="150"/></a><div class="newprod_name" style="text-align:center; padding:10px 0 0 0"><a   title="仓库实景图" >仓库实景图</a></div></div><div class="blankbar1"></div></div></td></tr></table></div><script type="text/javascript">new Marquee("nana",2,1,1200,230,50,0,0,52);</script></div></div></div></div>
+  
+  <?php include 'footer.php';?>
+  
+  </body></html>
